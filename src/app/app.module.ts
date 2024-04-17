57d06745507801a5e2df39a5da27855e4e73f8c7
elmaroufa <salyabbo@gmail.com>
2024-04-17 16:15:51 +0100

    feature directive permission for list role and dynamic component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsModule } from './products/products.module';
 import { CopyrightDirective } from './copyright.directive';
+import { NumericDirective } from './numeric.directive';
+import { PermissionDirective } from './permission.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CopyrightDirective
+    CopyrightDirective,
+    NumericDirective,
+    PermissionDirective
   ],
   imports: [
     BrowserModule,
